c4ed7c2ac7c80b9610495c0a05a00e840f83f832
Callum Inglis <callum.inglis@4oh4.co.uk>
2023-07-22 14:24:25 +0100

    Working SD Card:

--- _Testing/_Testing.ino
+++ _Testing/_Testing.ino
@@ -1,17 +1,21 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
 #include "config.h"
 
-// Device & Server Config
-const char* UUID = ""; // Auto-filled from MAC Address during setup()
+// SD Card
+const int SdChipSelect = D4;
+File myFile;
 
 // Stateful
 bool isRecording = false;
+bool isMotion = false;
 
 // Thresholds
 double accelerationZeroThreshold = 3;
@@ -20,25 +24,63 @@ int serialBaud = 115200;
 
 Adafruit_MPU6050 mpu;
 HTTPClient http;
+WiFiClient client;
 ESP8266WebServer webserver(80);
 
 /** SETUP **/
 bool setupWifi() {
-  WiFi.config();
+  WiFi.hostname(getDeviceUID());
   WiFi.begin(WIFI_SSID, WIFI_PASS); 
 
-  while (WiFi.status() != WL_CONNECTED && wifiTimeout > 0) {
-    Serial.println("Waiting for connection");
-    delay(5000);
-    
-    wifiTimeout--;
+  Serial.print("WiFi Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  return true;
+}
+
+void setupBuzzer() {
+  pinMode(BUZZER, OUTPUT);
+}
+
+bool setupSD() {
+  Serial.print("Initializing SD card... ");
+
+  if (!SD.begin(SdChipSelect)) {
+    Serial.println("initialization failed!");
+    return false;
   }
 
-  return WiFi.status() != WL_CONNECTED;
+  Serial.println("initialization done.");
+  return true;
+}
+
+void readSD() {
+  char* path = "/httpdocs/index.html";
+  myFile = SD.open(path);
+  if (myFile) {
+    Serial.println(path);
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+
+  } else {
+    // if the file didn't open, print an error:
+    Serial.print("error opening ");
+    Serial.println(path);
+  }
 }
 
 String getDeviceUID() {
-  return String(ESP.getChipId());
+  return "TIMECUBE-" + String(ESP.getChipId());
 }
 
 // TODO Shake detection for start/top
@@ -50,6 +92,50 @@ String getDeviceUID() {
 
 // TODO Real Time Clock?
 
+void blink(uint8_t Pin, int Count, int Duration) {
+  for (int i = 0; i < Count; i++) {
+    digitalWrite(Pin, HIGH); 
+    delay(Duration/2);
+    digitalWrite(BUZZER, LOW);
+    delay(Duration/2);
+  }
+}
+
+void motionDetected() {
+  Serial.print("Motion Detected... ");
+  int timeout = 10;
+
+  if (isRecording) {
+    Serial.println("Stopping Recording");
+    blink(BUZZER, 2, 150);
+  } else {
+    Serial.println("Starting Recording");
+    blink(BUZZER, 1, 150);
+  }
+  isRecording = !isRecording;
+
+  // Work out Direction
+  sensors_event_t a, g, temp;
+  String direction = "";
+
+  while (direction == "" && timeout > 0) {
+    delay(100);
+    timeout--;
+
+    mpu.getEvent(&a, &g, &temp);
+    direction = getFace(a.acceleration.x, a.acceleration.y, a.acceleration.z);
+  }
+
+  if (direction == "") {
+    Serial.println("Failed to detect direction after motion");
+    blink(BUZZER, 1, 500);
+    return;
+  }
+
+  Serial.print("Direction: ");
+  Serial.println(direction);
+}
+
 void setup() {
   Serial.begin(serialBaud);
 
@@ -60,20 +146,39 @@ void setup() {
   }
   Serial.println("MPU6050 Found!");
 
+  //setupt motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(30);
+  mpu.setMotionDetectionDuration(80);
+  mpu.setInterruptPinLatch(false);	// Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+
   // Setup Wifi
   if (!setupWifi()) {
     Serial.println("Failed to setup wifi");
     return;
   }
-  Serial.println("WIFI Connected!");
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setup SD Card
+  if (!setupSD()) {
+    Serial.println("Failed to setup SD Card");
+    return;
+  }
+  readSD();
 
   // Setup Device ID
-  UUID = getDeviceUID();
   Serial.print("Device UUID: ");
-  Serial.println(UUID);
+  Serial.println(getDeviceUID());
+
+  // Setup Buzzer
+  setupBuzzer();
 
   // Reset Recording State
   isRecording = false;
+  isMotion = false;
 
   // Start Web Server
   webserver.on("/", httpIndex);
@@ -84,25 +189,26 @@ void loop() {
   /* Listen for Web Requests */
   webserver.handleClient(); 
 
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  /* Look for Motion */
+  if(mpu.getMotionInterruptStatus()) {
+    /* Get new sensor events with the readings */
+    motionDetected();
 
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+    // /* Print out the values */
+    // Serial.print("Acceleration X: ");
+    // Serial.print(a.acceleration.x);
+    // Serial.print(", Y: ");
+    // Serial.print(a.acceleration.y);
+    // Serial.print(", Z: ");
+    // Serial.print(a.acceleration.z);
+    // Serial.println(" m/s^2");
 
+    
 
-  Serial.print("Direction: ");
-  Serial.println(getFace(a.acceleration.x, a.acceleration.y, a.acceleration.z));
+    Serial.println("");
+  }
 
-  Serial.println("");
-  delay(1500);
+  delay(100);
 }
 
 /**
@@ -158,11 +264,11 @@ String getFace(double x, double y, double z) {
 }
 
 void startRecordingTask(String faceName) {
-  http.begin(API + "recording/start");
+  http.begin(client, API + "recording/start");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   http.addHeader("APIKEY", APIKEY);
 
-  String postData = "DiceUUID=" + UUID + "&FaceName=" + faceName;
+  String postData = "DiceUUID=" + getDeviceUID() + "&FaceName=" + faceName;
 
   int httpCode = http.POST(postData);
   String payload = http.getString();
@@ -180,5 +286,5 @@ void stopRecordingTask() {
 
 /** HTTP Server **/
 void httpIndex() { 
-  webserver.send(200, "text/plain", "Hello World! Device UUID: " + UUID); 
+  webserver.send(200, "text/plain", "Hello World! Device UUID: " + getDeviceUID()); 
 }
