4015637c5dcd79c0020a0644d81511c43dba0366
Callum Inglis <callum.inglis@4oh4.co.uk>
2023-07-22 15:56:21 +0100

    Working Start/Stop Detection

--- _Testing/_Testing.ino
+++ _Testing/_Testing.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <stdio.h>
+#include <math.h>
 #include <SPI.h>
 #include <SD.h>
 #include <ESP8266HTTPClient.h>
@@ -16,11 +18,13 @@ File myFile;
 // Stateful
 bool isRecording = false;
 bool isMotion = false;
+int slack = 0;
 
 // Thresholds
 double accelerationZeroThreshold = 3;
 int wifiTimeout = 10;
-int serialBaud = 115200;
+int serialBaud = 9600;
+int searchDirectionTimeout = SEARCH_DIRECTION_THRESHOLD * 1000 / SEARCH_DIRECTION_INTERVAL;
 
 Adafruit_MPU6050 mpu;
 HTTPClient http;
@@ -32,7 +36,7 @@ bool setupWifi() {
   WiFi.hostname(getDeviceUID());
   WiFi.begin(WIFI_SSID, WIFI_PASS); 
 
-  Serial.print("WiFi Connecting");
+  Serial.print("[WiFi] :: Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -40,47 +44,103 @@ bool setupWifi() {
   }
   Serial.println();
 
+  // WiFi 
+  Serial.print("[WiFi] :: Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
   return true;
 }
 
 void setupBuzzer() {
   pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, LOW);
 }
 
-bool setupSD() {
-  Serial.print("Initializing SD card... ");
+bool setupMPU() {
+  Serial.println("[MPU6050] :: Connecting...!");
 
-  if (!SD.begin(SdChipSelect)) {
-    Serial.println("initialization failed!");
+  if (!mpu.begin()) {
+    Serial.println("[MPU6050] :: Failed to find MPU6050 chip");
     return false;
   }
+  Serial.println("[MPU6050] :: Found!");
 
-  Serial.println("initialization done.");
+  // Setup motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(MOTION_DETECTION_THRESHOLD);
+  mpu.setMotionDetectionDuration(MOTION_DETECTION_DURATION);
+  mpu.setInterruptPinLatch(false);	// Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+
+  Serial.println("[MPU6050] :: Configured!");
   return true;
 }
 
-void readSD() {
-  char* path = "/httpdocs/index.html";
-  myFile = SD.open(path);
-  if (myFile) {
-    Serial.println(path);
+void setupWebServer() {
+  Serial.println("[webserver] :: Starting...");
 
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
+  webserver.on("/", httpIndex);
+  webserver.begin();
 
-  } else {
-    // if the file didn't open, print an error:
-    Serial.print("error opening ");
-    Serial.println(path);
-  }
+  Serial.println("[webserver] :: Started");
 }
+
+void setupInitVars() {
+  // Reset Recording State
+  isRecording = false;
+  isMotion = false;  
+}
+
+// bool setupSD() {
+//   Serial.print("Initializing SD card... ");
+
+//   if (!SD.begin(SdChipSelect)) {
+//     Serial.println("initialization failed!");
+//     return false;
+//   }
+
+//   Serial.println("initialization done.");
+//   return true;
+// }
+
+// void readSD() {
+//   String path = "/httpdocs/index.html";
+//   myFile = SD.open(path);
+//   if (myFile) {
+//     Serial.println(path);
+
+//     // read from the file until there's nothing else in it:
+//     while (myFile.available()) {
+//       Serial.write(myFile.read());
+//     }
+//     // close the file:
+//     myFile.close();
+
+//   } else {
+//     // if the file didn't open, print an error:
+//     Serial.print("error opening ");
+//     Serial.println(path);
+//   }
+// }
 
 String getDeviceUID() {
   return "TIMECUBE-" + String(ESP.getChipId());
+}
+
+void setupNotice() {
+  // Device ID
+  Serial.print("[NOTICE] :: Device UUID: ");
+  Serial.println(getDeviceUID());
+
+  // Search Duration Timeout
+  Serial.print("[NOTICE] :: We will make ");
+  Serial.print(searchDirectionTimeout);
+  Serial.println(" attempts to determine direction/orientation after recording start.");
+
+  // Ready To Go
+  blink(BUZZER, 2, 100);
+  Serial.println();
 }
 
 // TODO Shake detection for start/top
@@ -102,87 +162,93 @@ void blink(uint8_t Pin, int Count, int Duration) {
 }
 
 void motionDetected() {
+  if (isMotion || slack > 0) { return; }
+
+  isMotion = true;
+  slack = SLACK;
+
   Serial.print("Motion Detected... ");
   int timeout = 10;
 
   if (isRecording) {
     Serial.println("Stopping Recording");
     blink(BUZZER, 2, 150);
+
+    // TODO Handle Stop Recording
+
+    delay(250);
+    
   } else {
     Serial.println("Starting Recording");
-    blink(BUZZER, 1, 150);
+    blink(BUZZER, 1, 300);
+
+    sensors_event_t a, g, temp;
+    String direction = "";
+
+    Serial.print("Direction");
+    while (direction == "" && searchDirectionTimeout > 0) {
+      delay(SEARCH_DIRECTION_INTERVAL);
+      searchDirectionTimeout--;
+
+      mpu.getEvent(&a, &g, &temp);
+      direction = getFace(a.acceleration.x, a.acceleration.y, a.acceleration.z);
+
+      Serial.print(".");
+    }
+
+    if (direction == "") {
+      Serial.println("Failed to detect direction after motion");
+      blink(BUZZER, 1, 2000);
+
+      isRecording = false;
+      isMotion = false;
+      slack = 0;
+
+      return;
+    }
+
+    
+    Serial.println(direction);
+    // TODO Log Start time
   }
+
   isRecording = !isRecording;
-
-  // Work out Direction
-  sensors_event_t a, g, temp;
-  String direction = "";
-
-  while (direction == "" && timeout > 0) {
-    delay(100);
-    timeout--;
-
-    mpu.getEvent(&a, &g, &temp);
-    direction = getFace(a.acceleration.x, a.acceleration.y, a.acceleration.z);
-  }
-
-  if (direction == "") {
-    Serial.println("Failed to detect direction after motion");
-    blink(BUZZER, 1, 500);
-    return;
-  }
-
-  Serial.print("Direction: ");
-  Serial.println(direction);
+  isMotion = false;
 }
 
 void setup() {
   Serial.begin(serialBaud);
 
-  // Init Gyro
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    return;
-  }
-  Serial.println("MPU6050 Found!");
-
-  //setupt motion detection
-  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
-  mpu.setMotionDetectionThreshold(30);
-  mpu.setMotionDetectionDuration(80);
-  mpu.setInterruptPinLatch(false);	// Keep it latched.  Will turn off when reinitialized.
-  mpu.setInterruptPinPolarity(true);
-  mpu.setMotionInterrupt(true);
-
-  // Setup Wifi
-  if (!setupWifi()) {
-    Serial.println("Failed to setup wifi");
-    return;
-  }
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // Setup SD Card
-  if (!setupSD()) {
-    Serial.println("Failed to setup SD Card");
-    return;
-  }
-  readSD();
-
-  // Setup Device ID
-  Serial.print("Device UUID: ");
-  Serial.println(getDeviceUID());
-
   // Setup Buzzer
   setupBuzzer();
 
-  // Reset Recording State
-  isRecording = false;
-  isMotion = false;
+  // Setup MPU6050 (Gyro)1
+  if (!setupMPU()) {
+    Serial.println("[ERROR] :: Failed to setup MPU6050");
+    return;
+  }
 
-  // Start Web Server
-  webserver.on("/", httpIndex);
-  webserver.begin();
+  // Setup Wifi
+  if (!setupWifi()) {
+    Serial.println("[ERROR] :: Failed to setup wifi");
+    return;
+  }
+
+  // Setup Web Server
+  setupWebServer();
+
+  // Initialise
+  setupInitVars();
+  
+  // // Setup SD Card
+  // if (!setupSD()) {
+  //   Serial.println("Failed to setup SD Card");
+  //   return;
+  // }
+  // readSD();
+
+  // Ready to Go
+  setupNotice();
 }
 
 void loop() {
@@ -202,13 +268,10 @@ void loop() {
     // Serial.print(", Z: ");
     // Serial.print(a.acceleration.z);
     // Serial.println(" m/s^2");
-
-    
-
-    Serial.println("");
   }
 
   delay(100);
+  slack = max(slack - 1, 0);
 }
 
 /**
@@ -263,7 +326,7 @@ String getFace(double x, double y, double z) {
   return face;
 }
 
-void startRecordingTask(String faceName) {
+void startRecordingTask(char* faceName) {
   http.begin(client, API + "recording/start");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   http.addHeader("APIKEY", APIKEY);
