44f6230f5a0e6b429d5bd4652887a495fcfcdfb5
Callum Inglis <callum.inglis@4oh4.co.uk>
2023-08-09 22:44:57 +0100

    Seed database with useful info

--- Server/database/sql/Seed Database.sql
+++ Server/database/sql/Seed Database.sql
@@ -1,84 +1,93 @@
-﻿INSERT INTO organisation ("name") VALUES ('Test Organisation');
+﻿INSERT INTO organisation ("name") VALUES ('4oh4 Inc.');
 
 INSERT INTO "user" (organisation, "name") VALUES (
-     (SELECT organisationid FROM organisation WHERE "name" = 'Test Organisation')
-    ,'Test User'
+     (SELECT organisationid FROM organisation WHERE "name" = '4oh4 Inc.')
+    ,'Callum Inglis'
 );
 
 INSERT INTO dice (uuid, "name", faces) VALUES (
 	 'TIMECUBE-6716273'
-    ,'Test Dice 1'
+    ,'Clear Dodecahedron'
     ,12
 );
 
 INSERT INTO diceface (dice, facenumber) VALUES
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 1),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 2),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 3),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 4),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 5),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 6),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 7),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 8),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 9),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 10),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 11),
-    ((SELECT diceid FROM dice WHERE "name" = 'Test Dice 1'), 12);
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 1),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 2),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 3),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 4),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 5),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 6),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 7),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 8),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 9),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 10),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 11),
+    ((SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron'), 12);
 
-INSERT INTO tasktype ("name") VALUES ('Basic Task');
+INSERT INTO tasktype ("name") VALUES ('Basic Task (No Integration)');
 
 INSERT INTO tasks (tasktype, "name", organisation) VALUES 
     (
-        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task')
-        ,'Test Task 1'
-        ,(SELECT organisationid FROM organisation WHERE "name" = 'Test Organisation')
+        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task (No Integration)')
+        ,'Building Timer Cube'
+        ,(SELECT organisationid FROM organisation WHERE "name" = '4oh4 Inc.')
     ),
     (
-        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task')
-        ,'Test Task 2'
-        ,(SELECT organisationid FROM organisation WHERE "name" = 'Test Organisation')
+        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task (No Integration)')
+        ,'AZ-104: Microsoft Azure Administrator (Learning)'
+        ,(SELECT organisationid FROM organisation WHERE "name" = '4oh4 Inc.')
     )
     ,
     (
-        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task')
-        ,'Test Task 3'
-        ,(SELECT organisationid FROM organisation WHERE "name" = 'Test Organisation')
+        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task (No Integration)')
+        ,'Example Client Feature 2'
+        ,(SELECT organisationid FROM organisation WHERE "name" = '4oh4 Inc.')
     ),
     (
-        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task')
-        ,'Test Task 4'
-        ,(SELECT organisationid FROM organisation WHERE "name" = 'Test Organisation')
+        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task (No Integration)')
+        ,'Example Client Feature 3'
+        ,(SELECT organisationid FROM organisation WHERE "name" = '4oh4 Inc.')
+    ),
+    (
+        (SELECT tasktypeid FROM tasktype WHERE "name" = 'Basic Task (No Integration)')
+        ,'Example Client Feature 1'
+        ,(SELECT organisationid FROM organisation WHERE "name" = '4oh4 Inc.')
     )
 ;
 
 INSERT INTO userdice ("user", dice) VALUES (
-     (SELECT userid FROM "user" WHERE "name" = 'Test User')
-    ,(SELECT diceid FROM dice WHERE "name" = 'Test Dice 1')
+     (SELECT userid FROM "user" WHERE "name" = 'Callum Inglis')
+    ,(SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron')
 );
 
 INSERT INTO dicefacetask (diceface, task) VALUES 
     (
-        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Test Dice 1') AND "facenumber" = '1')
-        ,(SELECT taskid FROM tasks WHERE "name" = 'Test Task 1')
+        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron') AND "facenumber" = '1')
+        ,(SELECT taskid FROM tasks WHERE "name" = 'Building Timer Cube')
     ),
     (
-        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Test Dice 1') AND "facenumber" = '2')
-        ,(SELECT taskid FROM tasks WHERE "name" = 'Test Task 2')
+        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron') AND "facenumber" = '2')
+        ,(SELECT taskid FROM tasks WHERE "name" = 'AZ-104: Microsoft Azure Administrator (Learning)')
     )
     ,
     (
-        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Test Dice 1') AND "facenumber" = '3')
-        ,(SELECT taskid FROM tasks WHERE "name" = 'Test Task 3')
+        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron') AND "facenumber" = '3')
+        ,(SELECT taskid FROM tasks WHERE "name" = 'Example Client Feature 1')
     )
     ,
     (
-        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Test Dice 1') AND "facenumber" = '4')
-        ,(SELECT taskid FROM tasks WHERE "name" = 'Test Task 4')
+        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron') AND "facenumber" = '4')
+        ,(SELECT taskid FROM tasks WHERE "name" = 'Example Client Feature 2')
+    ),
+    (
+        (SELECT dicefaceid FROM diceface WHERE dice = (SELECT diceid FROM dice WHERE "name" = 'Clear Dodecahedron') AND "facenumber" = '5')
+        ,(SELECT taskid FROM tasks WHERE "name" = 'Example Client Feature 3')
     )
 ;
 
 CREATE EXTENSION "uuid-ossp";
 INSERT INTO apikey (apikey, "user") VALUES (
     (SELECT uuid_generate_v4()::text), 
-    (SELECT userid FROM "user" WHERE "name" = 'Test User')
+    (SELECT userid FROM "user" WHERE "name" = 'Callum Inglis')
 );
