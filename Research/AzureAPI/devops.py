030e4c58cec04636f320fa0a1bb6f8b57e04853e
Callum Inglis <callum.inglis@4oh4.co.uk>
2023-08-19 20:48:45 +0100

    Testing Azure Devops Package; Not working very well and few good examples available

---
+++ Research/AzureAPI/devops.py
@@ -0,0 +1,30 @@
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+import pprint
+
+# Fill in with your personal access token and org URL
+personal_access_token = ''
+organization_url = 'https://dev.azure.com/4oh4ltd'
+
+# Create a connection to the org
+credentials = BasicAuthentication('', personal_access_token)
+connection = Connection(base_url=organization_url, creds=credentials)
+
+# Get a client (the "core" client provides access to projects, teams, etc)
+core_client = connection.clients.get_core_client()
+
+# Get the first page of projects
+get_projects_response = core_client.get_projects()
+index = 0
+while get_projects_response is not None:
+    for project in get_projects_response:
+        pprint.pprint("[" + str(index) + "] " + project.name)
+        index += 1
+
+    if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
+        # Get the next page of projects
+        get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
+        
+    else:
+        # All projects have been retrieved
+        get_projects_response = None
